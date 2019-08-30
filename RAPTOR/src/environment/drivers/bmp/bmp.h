6405162e91bd4b4655a47315e1ed192553ce1ad4
Sean Widmier <seanwidmier@gmail.com>
2019-08-30 16:54:51 -0500

    upgrade to bmp280 driver

--- RAPTOR/src/environment/drivers/bmp/bmp.h
+++ RAPTOR/src/environment/drivers/bmp/bmp.h
@@ -1,25 +1,24 @@
 /*
- * bmp.h - 
- * Library written for use with Adafruit BMP180 sensor.
+ * bmp.h -
+ * Class written for use with Adafruit BMP180 sensor.
  * Utilizes Adafruit BMP library
  * Part of the RAPTOR project, authors: Sean Widmier, Colin Oberthur
 */
 #ifndef BMP_H_
 #define BMP_H_
 
-#include <Adafruit_BMP085_U.h> // external adafruit library
+#include <Adafruit_BMP280.h>
 #include <Adafruit_Sensor.h>
-#include "Arduino.h"
 
 class BMP
-    : public Adafruit_BMP085_Unified
+    : public Adafruit_BMP280
 {
 public:
-  BMP() : Adafruit_BMP085_Unified(10085) {}
-  bool init(uint8_t fs);
-  bool update(void);
+  BMP() : Adafruit_BMP280() {}
+  bool init(bool set_baseline);
+  float getAltitude(void);
 
-  float baseline, pressure, temperature, altitude;
+  float baseline;
 };
 
 #endif /* BMP_H_ */
